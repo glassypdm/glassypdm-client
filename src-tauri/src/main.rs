2bcb4b61cc1f7a899851b354a448272b3b7097b5
Joshua Tenorio <joshliam09@gmail.com>
2023-08-29 00:25:32 -0700

    mwe file dialog for directory select

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,20 +1,47 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::fs::File;
-use std::io::prelude::*;
-use std::path::Path;
+use tauri::api::dialog;
+use tauri::api::path::local_data_dir;
+use tauri::{CustomMenuItem, Menu, MenuItem, Submenu};
 
-
+static mut local_path: &str = "";
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+    let output: String = format!("Hello, {}! You've been greeted from Rust!", name);
+    println!("helasdfasdfasdfsdf");
+    return output;
+}
+
+#[tauri::command]
+fn get_directory(path: &str) {
+    println!("path: {}", path);
 }
 
 fn main() {
+    let open = CustomMenuItem::new("open".to_string(), "Open");
+    let fileMenu = Submenu::new("File", Menu::new().add_item(open));
+    let menu = Menu::new()
+      .add_submenu(fileMenu)
+      .add_native_item(MenuItem::Separator)
+      .add_native_item(MenuItem::Quit);
+
     tauri::Builder::default()
+        .menu(menu)
+        .on_menu_event(|event| match event.menu_item_id() {
+        "open" => {
+            dialog::FileDialogBuilder::default()
+            .add_filter("Markdown", &["md"])
+            .pick_folder(|path_buf| match path_buf {
+                Some(p) => { println!("{}", p.display()); }
+                _ => {}
+            });
+        }
+        _ => {}
+        })
         .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![get_directory])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
+
 import "./App.css";
 
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
   const [name, setName] = useState("");
+  const [path, setPath] = useState("");
 
   async function greet() {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
     setGreetMsg(await invoke("greet", { name }));
+  }
+
+  async function directory() {
+    setGreetMsg(await invoke("get_directory", { path }));
+
+
   }
 
   return (
