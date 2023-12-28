f1adbe6071af429eaaf1dbf6e9713004257fd7df
Joshua Tenorio <joshliam09@gmail.com>
2023-12-28 14:10:09 -0700

    chore: move upload_files fn to upload.rs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -11,16 +11,14 @@ use std::path::Path;
 use tauri::Manager;
 use std::fs::{File, self};
 use std::io;
-use reqwest::{Client, Response};
-use reqwest::multipart::*;
+use reqwest::Client;
 use tauri_plugin_log::LogTarget;
 use log::{info, trace, error};
 use futures::{stream, StreamExt};
 use crate::sync::{hash_dir, sync_server};
 use crate::settings::{update_server_url, get_server_url, get_project_dir, update_project_dir};
-use crate::types::{UploadStatusPayload, Change, S3FileLink, FileUploadStatus, ReqwestError, DownloadFile, DownloadInformation, DownloadStatusPayload};
-use crate::upload::update_upload_list;
-use crate::util::get_file_as_byte_vec;
+use crate::types::{S3FileLink, ReqwestError, DownloadFile, DownloadInformation, DownloadStatusPayload};
+use crate::upload::{update_upload_list, upload_files};
 
 const CONCURRENT_REQUESTS: usize = 4;
 
@@ -126,53 +124,6 @@ fn download_s3_file(app_handle: tauri::AppHandle, link: S3FileLink) {
 fn delete_file(app_handle: tauri::AppHandle, file: String) {
     let path = get_project_dir(app_handle) + file.as_str();
     let _ = fs::remove_file(path);
-}
-
-#[tauri::command]
-async fn upload_files(app_handle: tauri::AppHandle, files: Vec<Change>, commit: u64, server_url: String) -> Result<(), ReqwestError> {
-    let client: Client = reqwest::Client::new();
-    let url: String = server_url.to_string() + "/ingest";
-
-    let project_dir = get_project_dir(app_handle.clone());
-    let total: u32 = files.len().try_into().unwrap();
-    let mut uploaded: u32 = 0;
-    for change in files {
-        let file = change.file;
-        let path: String = file.path;
-        let rel_path = path.replace(&project_dir, "");
-
-        // create request
-        let mut form: Form = reqwest::multipart::Form::new()
-        .text("project", 0.to_string())
-        .text("commit", commit.to_string())
-        .text("path", rel_path.clone())
-        .text("size", file.size.to_string())
-        .text("change", (change.change as u64).to_string())
-        .text("hash", file.hash);
-        // TODO consider using file.change instead of file.file.size to see if we delete the file
-        if file.size != 0 {
-            let content: Vec<u8> = get_file_as_byte_vec(&path);
-            form = form.part("key", Part::bytes(content).file_name(rel_path.clone()));
-        }
-
-        // send request
-        let res: Response = client.post(url.to_string())
-            .multipart(form)
-            .send()
-            .await?;
-        
-        // get s3 key and store it
-        let data = res.json::<FileUploadStatus>().await?;
-        let mut s3 = "oops".to_string();
-        if data.result {
-            s3 = data.s3key;
-        }
-
-        // emit status event
-        uploaded += 1;
-        app_handle.emit_all("uploadStatus", UploadStatusPayload { uploaded, total, s3, rel_path }).unwrap();
-    }
-    Ok(())
 }
 
 fn main() {
--- src-tauri/src/upload.rs
+++ src-tauri/src/upload.rs
@@ -2,9 +2,59 @@ use std::fs::{File, self};
 use std::io::Write;
 use std::path::PathBuf;
 use log::{error, info};
+use tauri::Manager;
+use reqwest::{Client, Response};
+use reqwest::multipart::*;
+use crate::settings::{get_app_local_data_dir, get_project_dir};
+use crate::types::{UploadStatusPayload, Change, FileUploadStatus, ReqwestError};
+use crate::util::get_file_as_byte_vec;
 
-use crate::types::Change;
-use crate::settings::get_app_local_data_dir;
+#[tauri::command]
+pub async fn upload_files(app_handle: tauri::AppHandle, files: Vec<Change>, commit: u64, server_url: String) -> Result<(), ReqwestError> {
+    let client: Client = reqwest::Client::new();
+    let url: String = server_url.to_string() + "/ingest";
+
+    let project_dir = get_project_dir(app_handle.clone());
+    let total: u32 = files.len().try_into().unwrap();
+    let mut uploaded: u32 = 0;
+    for change in files {
+        let file = change.file;
+        let path: String = file.path;
+        let rel_path = path.replace(&project_dir, "");
+
+        // create request
+        let mut form: Form = reqwest::multipart::Form::new()
+        .text("project", 0.to_string())
+        .text("commit", commit.to_string())
+        .text("path", rel_path.clone())
+        .text("size", file.size.to_string())
+        .text("change", (change.change as u64).to_string())
+        .text("hash", file.hash);
+        // TODO consider using file.change instead of file.file.size to see if we delete the file
+        if file.size != 0 {
+            let content: Vec<u8> = get_file_as_byte_vec(&path);
+            form = form.part("key", Part::bytes(content).file_name(rel_path.clone()));
+        }
+
+        // send request
+        let res: Response = client.post(url.to_string())
+            .multipart(form)
+            .send()
+            .await?;
+        
+        // get s3 key and store it
+        let data = res.json::<FileUploadStatus>().await?;
+        let mut s3 = "oops".to_string();
+        if data.result {
+            s3 = data.s3key;
+        }
+
+        // emit status event
+        uploaded += 1;
+        app_handle.emit_all("uploadStatus", UploadStatusPayload { uploaded, total, s3, rel_path }).unwrap();
+    }
+    Ok(())
+}
 
 #[tauri::command]
 pub fn update_upload_list(app_handle: tauri::AppHandle, changes: Vec<Change>) -> Vec<Change> {
