6cffa6c7a29d70f11ce3eb6df512138e7be5609c
Joshua Tenorio <joshliam09@gmail.com>
2024-01-16 15:58:51 -0700

    better idea of project management reowrk

---
+++ src-tauri/src/project.rs
@@ -0,0 +1,9 @@
+// stores to manage:
+// - base.dat
+// - compare.dat
+// - toUpload.dat
+// - toDownload.dat
+// - config.dat
+// functions that generate the above absolute paths
+// hash a directory all at once (like sync)
+// hash a specific file
