249a8abb6418d37d45bc75da2bebca2d171d5654
Joshua Tenorio <joshliam09@gmail.com>
2024-03-17 09:59:29 -0700

    server input wip

--- src/routes/serversetup.tsx
+++ src/routes/serversetup.tsx
@@ -5,16 +5,21 @@ import { Button } from '@/components/ui/button'
 import { createFileRoute } from '@tanstack/react-router'
 import { z } from 'zod'
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { useState } from "react";
+import { Loader2 } from "lucide-react"
 
 export const Route = createFileRoute('/serversetup')({
     component: ServerSetup,
 })
 
 const formSchema = z.object({
-    serverURL: z.string().url({ message: "Invalid url." })
+    serverURL: z.string().url({ message: "Invalid url." }).startsWith("http", { message: "Invalid url." })
 })
 
 function ServerSetup() {
+    const [submitStatus, setSubmitStatus] = useState(false);
+    const [submitText, setSubmitText] = useState(<p>Submit</p>);
+    
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
@@ -22,24 +27,34 @@ function ServerSetup() {
           },
     })
 
-    function onSubmit(values: z.infer<typeof formSchema>) {
+    async function onSubmit(values: z.infer<typeof formSchema>) {
         console.log(values)
+        setSubmitStatus(true);
+        setSubmitText(<Loader2 className="h-4 w-4 animate-spin" />);
 
         // fetch data from server/daijin-config
-        // if its valid, 
+        const data = await fetch(values.serverURL + "/daijin-config")
+        // FIXME cors
+        console.log(data)
+        setSubmitStatus(false);
+        setSubmitText(<p>Submit</p>)
+        // if its valid, store server url (and clerk key?)
+        // and redirect to login/register
       }
 
 
   return (
-    <div>
+    <div className="flex flex-col place-items-center">
+        <h1 className="text-4xl my-12">daijinCAD</h1>
+        <h2 className="text-2xl">Set the Server URL</h2>
         <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="w-3/4 my-12 space-y-4">
                 <FormField
                     control={form.control}
                     name="serverURL"
                     render={({ field }) => (
                         <FormItem>
-                            <FormLabel>ServerURL</FormLabel>
+                            <FormLabel className="text-xl">Server URL</FormLabel>
                             <FormControl>
                                 <Input  {...field} />
                             </FormControl>
@@ -47,7 +62,7 @@ function ServerSetup() {
                         </FormItem>
                     )}
                     />
-                    <Button type="submit">Submit</Button>
+                    <Button type="submit" disabled={submitStatus}>{submitText}</Button>
             </form>
         </Form>
     </div>
