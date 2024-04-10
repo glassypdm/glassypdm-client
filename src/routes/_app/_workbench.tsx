6e23a989710183229427b0c74e306c3f5431621f
Joshua Tenorio <joshliam09@gmail.com>
2024-04-10 03:36:21 -0700

    feat: update about dialog

--- src/routes/_app/_workbench.tsx
+++ src/routes/_app/_workbench.tsx
@@ -1,3 +1,4 @@
+import { getVersion, getTauriVersion } from '@tauri-apps/api/app';
 import { Link, Outlet, createFileRoute, useRouterState } from '@tanstack/react-router'
 import { useAuth } from "@clerk/clerk-react"
 import { Button } from '@/components/ui/button';
@@ -5,12 +6,15 @@ import { Separator } from '@/components/ui/separator';
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbSeparator } from '@/components/ui/breadcrumb';
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
 import { Menu } from 'lucide-react';
+import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
 
 
 export const Route = createFileRoute('/_app/_workbench')({
   component: WorkbenchLayout
 })
 
+const GLASSYPDM_VERSION = await getVersion();
+const TAURI_VERSION = await getTauriVersion();
 
 function WorkbenchLayout() {
     //const router = useRouterState();
@@ -30,20 +34,38 @@ function WorkbenchLayout() {
               <BreadcrumbItem>SDM-25</BreadcrumbItem>
             </BreadcrumbList>
             </Breadcrumb>
+            <Dialog>
             <DropdownMenu>
               <DropdownMenuTrigger className='max-w-fit justify-self-end mx-4' asChild>
                 <Button variant='outline'><Menu className='mr-2 h-4 w-4' />Open</Button>
               </DropdownMenuTrigger>
               <DropdownMenuContent className='mx-4'>
+                <DialogTrigger asChild>
                 <DropdownMenuItem>About</DropdownMenuItem>
+                </DialogTrigger>
                 <DropdownMenuSeparator className='mx-2'/>
-                <DropdownMenuItem>Client Settings</DropdownMenuItem>
-                <DropdownMenuItem>Organization Settings</DropdownMenuItem>
-                <DropdownMenuSeparator className='mx-2'/>
+                <DropdownMenuItem>Settings</DropdownMenuItem>
                 <DropdownMenuItem>Profile</DropdownMenuItem>
+                <DropdownMenuSeparator className='mx-2'/>
                 <DropdownMenuItem onClick={() => signOut()}>Sign Out</DropdownMenuItem>
               </DropdownMenuContent>
             </DropdownMenu>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>About glassyPDM</DialogTitle>
+              </DialogHeader>
+              glassyPDM version: v{GLASSYPDM_VERSION}
+              <br />
+              Tauri version: v{TAURI_VERSION}
+              <p>Client source code: <a href='https://github.com/joshtenorio/glassypdm-client' target='_blank' className='underline'>GitHub</a></p>
+              <p>
+              The glassyPDM client is released under the GNU General Public License (GPL) version 3 or later version.
+              </p>
+              <DialogFooter className='flex flex-row'>
+              <Button>Report Bug</Button>
+            </DialogFooter>
+            </DialogContent>
+            </Dialog>
           </div>
         <Separator />
         <Outlet />
