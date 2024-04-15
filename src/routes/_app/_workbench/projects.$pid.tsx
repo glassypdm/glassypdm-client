b02be7a3107e55ba0a63dbaa7e3e5523f6880dd8
Joshua Tenorio <joshliam09@gmail.com>
2024-04-15 16:24:46 -0700

    wip project page

--- src/routes/_app/_workbench/projects.$pid.tsx
+++ src/routes/_app/_workbench/projects.$pid.tsx
@@ -1,10 +1,24 @@
 import { createFileRoute } from '@tanstack/react-router'
+import Database from "@tauri-apps/plugin-sql";
 
 export const Route = createFileRoute('/_app/_workbench/projects/$pid')({
   component: Project,
+
+  loader: async () => {
+      const db = await Database.load("sqlite:testing.db")
+      const result = await db.select(
+          "SELECT debug_url FROM server WHERE active = 1" // TODO url
+      );
+      const url = (result as any)[0].debug_url;
+      // TODO fetch /project?pid={pid}
+      return {
+          url: url
+      }
+  }
 })
 
 function Project() {
+    const loaderData = Route.useLoaderData();
     const { pid } = Route.useParams();
   return <div>project ID: {pid}</div>
 }
--- src/routes/_app/_workbench/settings.tsx
+++ src/routes/_app/_workbench/settings.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { createFileRoute } from "@tanstack/react-router";
 
@@ -9,7 +10,10 @@ function Settings() {
     return (
         <div className="flex flex-row">
             <div className="flex flex-col mx-4">
-                <h1 className="text-2xl font-semibold">Settings</h1>
+                <h1 className="text-2xl font-semibold mb-4">Settings</h1>
+                <Button variant={"ghost"}>Client</Button>
+                <Button variant={"ghost"}>Teams</Button>
+                <Button variant={"ghost"}>Account</Button>
             </div>
             <div className="flex flex-col grow space-y-4 mx-4">
                 <Card>
