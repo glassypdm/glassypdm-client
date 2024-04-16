d01cb33fe9b7b9ebca98d00fd61cd2f1f63c6583
Joshua Tenorio <joshliam09@gmail.com>
2024-04-15 23:18:29 -0700

    ui scaffolding for settings page

--- src/routes/_app/_workbench/settings.tsx
+++ src/routes/_app/_workbench/settings.tsx
@@ -1,5 +1,7 @@
 import { Button } from "@/components/ui/button";
-import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import { Switch } from "@/components/ui/switch";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute('/_app/_workbench/settings')({
@@ -27,6 +29,26 @@ function Settings() {
                         <CardTitle>App Data</CardTitle>
                         <CardDescription>Manage your local app data.</CardDescription>
                     </CardHeader>
+                    <CardContent className="flex flex-row space-x-4">
+                    <Button>View App Data</Button>
+                    <Button>View App Logs</Button>
+                    <Button variant={"outline"}>Delete App Data</Button> {/** TODO alert dialog w/ destructive color */}
+                    </CardContent>
+                </Card>
+                <Card>
+                    <CardHeader>
+                        <CardTitle>Developer Settings</CardTitle>
+                        <CardDescription>Trick silicon into thinking.</CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                        <div className="flex flex-row space-x-4 items-center">
+                            <Label>Use Debug Server</Label>
+                            <Switch />
+                        </div>
+                    </CardContent>
+                    <CardFooter>
+                        <Button>Save</Button>
+                    </CardFooter>
                 </Card>
             </div>
         </div>
