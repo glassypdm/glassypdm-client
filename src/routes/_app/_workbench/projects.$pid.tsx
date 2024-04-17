536a4e95f4c89e21035c031343eb677defc194ee
Joshua Tenorio <joshliam09@gmail.com>
2024-04-17 03:50:11 -0700

    scaffold project page

--- src/routes/_app/_workbench/projects.$pid.tsx
+++ src/routes/_app/_workbench/projects.$pid.tsx
@@ -1,18 +1,23 @@
+import { Button } from '@/components/ui/button';
+import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, navigationMenuTriggerStyle } from '@/components/ui/navigation-menu';
+import { cn } from '@/lib/utils';
 import { createFileRoute } from '@tanstack/react-router'
 import Database from "@tauri-apps/plugin-sql";
 
 export const Route = createFileRoute('/_app/_workbench/projects/$pid')({
   component: Project,
 
-  loader: async () => {
+  loader: async ({ params }) => {
       const db = await Database.load("sqlite:glassypdm.db")
       const result = await db.select(
           "SELECT debug_url FROM server WHERE active = 1" // TODO url
       );
       const url = (result as any)[0].debug_url;
-      // TODO fetch /project?pid={pid}
+      const res = await fetch(url + "/project?pid=" + params.pid);
+      const data = await res.json();
       return {
-          url: url
+          url: url,
+          title: data.title
       }
   }
 })
@@ -20,5 +25,35 @@ export const Route = createFileRoute('/_app/_workbench/projects/$pid')({
 function Project() {
     const loaderData = Route.useLoaderData();
     const { pid } = Route.useParams();
-  return <div>project ID: {pid}</div>
+  return (
+    <div className='flex flex-col items-center'>
+        <NavigationMenu className='mt-2 mb-6'>
+          <NavigationMenuList className='grid grid-flow-col space-x-4'>
+            <NavigationMenuItem className>
+            <NavigationMenuLink className={cn(navigationMenuTriggerStyle(), 'text-4xl font-semibold justify-self-start')}>{loaderData.title}</NavigationMenuLink>
+            </NavigationMenuItem>
+            <NavigationMenuItem asChild>
+              <NavigationMenuLink className={cn(navigationMenuTriggerStyle(), 'text-md')}>Project Updates</NavigationMenuLink>
+            </NavigationMenuItem>
+            <NavigationMenuItem asChild>
+              <NavigationMenuLink className={cn(navigationMenuTriggerStyle(), 'text-md')}>Files</NavigationMenuLink>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <NavigationMenuLink className={cn(navigationMenuTriggerStyle(), 'text-md')}>Settings</NavigationMenuLink>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+      <div className='grid grid-cols-3 gap-6 p-4 h-64'>
+        <div className='flex flex-col gap-4'>
+          <Button className='grow'>Download Changes</Button>
+          <Button variant={"outline"}>Open Website</Button>
+        </div>
+        <Button className='flex h-full'>Sync</Button>
+        <div className='flex flex-col gap-4'>
+          <Button className='grow'>Upload Changes</Button>
+          <Button variant={"outline"}>Open Project Folder</Button>
+        </div>
+      </div>
+    </div>
+  )
 }
