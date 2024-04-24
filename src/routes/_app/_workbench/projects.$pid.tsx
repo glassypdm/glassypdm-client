2b1d1d4512da392846463807fea647449b507045
Joshua Tenorio <joshliam09@gmail.com>
2024-04-23 23:04:53 -0700

    add tabs to project page

--- src/routes/_app/_workbench/projects.$pid.tsx
+++ src/routes/_app/_workbench/projects.$pid.tsx
@@ -1,6 +1,8 @@
 import { Button } from '@/components/ui/button';
 import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, navigationMenuTriggerStyle } from '@/components/ui/navigation-menu';
+import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { cn } from '@/lib/utils';
+import { TabsContent } from '@radix-ui/react-tabs';
 import { createFileRoute } from '@tanstack/react-router'
 import Database from "@tauri-apps/plugin-sql";
 
@@ -26,27 +28,34 @@ function Project() {
     const loaderData = Route.useLoaderData();
     const { pid } = Route.useParams();
   return (
-    <div className='flex flex-col items-center'>
-        <NavigationMenu className='mt-2 mb-6'>
+    <div>
+      <Tabs defaultValue='home' className='flex flex-col items-center'>
+        <TabsList asChild>
+        <NavigationMenu className='mt-2 mb-6 bg-background'>
           <NavigationMenuList className='grid grid-flow-col space-x-4'>
+            <TabsTrigger value="home">
             <NavigationMenuItem>
             <NavigationMenuLink className={cn(navigationMenuTriggerStyle(), 'text-4xl font-semibold justify-self-start')}>{loaderData.title}</NavigationMenuLink>
             </NavigationMenuItem>
-            <NavigationMenuItem asChild>
+            </TabsTrigger>
+            <TabsTrigger value="updates">
+            <NavigationMenuItem>
               <NavigationMenuLink className={cn(navigationMenuTriggerStyle(), 'text-md')}>Project Updates</NavigationMenuLink>
             </NavigationMenuItem>
-            <NavigationMenuItem asChild>
+            </TabsTrigger>
+            <TabsTrigger value="files">
+            <NavigationMenuItem>
               <NavigationMenuLink className={cn(navigationMenuTriggerStyle(), 'text-md')}>Files</NavigationMenuLink>
             </NavigationMenuItem>
-            <NavigationMenuItem>
-              <NavigationMenuLink className={cn(navigationMenuTriggerStyle(), 'text-md')}>Settings</NavigationMenuLink>
-            </NavigationMenuItem>
+            </TabsTrigger>
           </NavigationMenuList>
         </NavigationMenu>
-      <div className='grid grid-cols-3 gap-8 p-4 h-64'>
+        </TabsList>
+        <TabsContent value="home">
+        <div className='grid grid-cols-3 gap-8 p-4 h-64'>
         <div className='flex flex-col gap-4'>
           <Button className='grow'>Download Changes</Button>
-          <Button variant={"outline"}>Open Website</Button>
+          <Button variant={"outline"}>Open in Website</Button>
         </div>
         <Button className='flex h-full'>Sync</Button>
         <div className='flex flex-col gap-4'>
@@ -54,6 +63,14 @@ function Project() {
           <Button variant={"outline"}>Open Project Folder</Button>
         </div>
       </div>
+        </TabsContent>
+        <TabsContent value="updates">
+          <p>WIP</p>
+        </TabsContent>
+        <TabsContent value="files">
+          <p>WIP</p>
+        </TabsContent>
+      </Tabs>
     </div>
   )
 }
