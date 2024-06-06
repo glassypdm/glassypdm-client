274aca9457b16f9fcd2de81673d21eecc0a55d55
Joshua Tenorio <joshliam09@gmail.com>
2024-06-05 18:24:34 -0700

    determine change type, need to test

--- src-tauri/migrations/1_init.sql
+++ src-tauri/migrations/1_init.sql
@@ -1,18 +1,20 @@
 CREATE TABLE server (url TEXT NOT NULL PRIMARY KEY, name TEXT, clerk_publickey TEXT, local_dir TEXT, active INTEGER, debug_url TEXT, debug_active INTEGER);
 CREATE TABLE project (pid INTEGER NOT NULL PRIMARY KEY, url TEXT NOT NULL, base_commit INTEGER, tracked_commit INTEGER);
 CREATE TABLE file (
+    fid INTEGER AUTOINCREMENT NOT NULL,
     filepath TEXT NOT NULL,
     pid INTEGER NOT NULL,
     num_chunks INTEGER,
     size INTEGER,
+    base_commit INTEGER DEFAULT -1,
     tracked_commit INTEGER DEFAULT -1,
     change_type INTEGER DEFAULT 1,
     in_fs INTEGER DEFAULT 1,
     PRIMARY KEY (filepath, pid));
 CREATE TABLE chunk (
+    cid INTEGER AUTOINCREMENT NOT NULL,
     filepath TEXT NOT NULL,
     chunk_num INTEGER NOT NULL,
-    tracked_commit INTEGER DEFAULT -1,
     curr_hash TEXT DEFAULT "",
     base_hash TEXT DEFAULT "",
     tracked_hash TEXT DEFAULT "",
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -50,7 +50,15 @@ async fn hash_dir(pid: i32, dir_path: PathBuf, pool: &Pool<Sqlite>) {
         let _ = sqlx::query(
             "
             INSERT INTO file(filepath, pid, num_chunks, size) VALUES(?, ?, ?, ?)
-            ON CONFLICT(filepath) DO UPDATE SET num_chunks = excluded.num_chunks, size = excluded.size, in_fs = 1;
+            ON CONFLICT(filepath) DO UPDATE SET num_chunks = excluded.num_chunks, size = excluded.size, in_fs = 1,
+            change_type = CASE WHEN (
+                SELECT COUNT(*) FROM chunk WHERE
+                chunk.filepath = excluded.filepath AND
+                chunk.curr_hash != chunk.base_hash AND
+                chunk.chunk_num < excluded.num_chunks
+                ORDER BY chunk_num LIMIT(excluded.num_chunks)
+            )
+            THEN 2 ELSE 0 END;
             "
         )
         .bind(file.path().to_str())
@@ -58,11 +66,9 @@ async fn hash_dir(pid: i32, dir_path: PathBuf, pool: &Pool<Sqlite>) {
         .bind(hash_list.len() as u32)
         .bind(file.metadata().unwrap().file_size() as u32)
         .execute(&*pool).await;
-
-        // TODO determine change type
     }
 
-    // update change type for files with in_fs = 0
+    // update change type for files with in_fs = 0 (deleted)
     let _ = sqlx::query(
         "UPDATE file SET change_type = 3 WHERE in_fs = 0;"
     ).execute(&*pool).await;
@@ -76,13 +82,18 @@ async fn hash_dir(pid: i32, dir_path: PathBuf, pool: &Pool<Sqlite>) {
 async fn sync_changes(pid: i32, name: String, state_mutex: State<'_, Mutex<Pool<Sqlite>>>) -> Result<(), ()> {
     let pool = state_mutex.lock().await;
     let glassy_dir = get_server_dir(&pool).await.unwrap();
-    let dir = glassy_dir + "\\" + &name;
+    let project_dir = glassy_dir + "\\" + &name; // TODO generalize based on OS
 
     // create folder if it does not exist
-    let _ = fs::create_dir_all(&dir);
-    println!("dir: {}", dir);
+    let _ = fs::create_dir_all(&project_dir);
+    println!("dir: {}", project_dir);
     hash_dir(pid, "dir".into(), &pool).await;
-    println!("done");
+    println!("hashing done");
+
+    // TODO get updated version of project
+    // client should send server a filepath and the base commit
+    // if there is a new version, server responds with chunk list and tracked commit
+    // otherwise, response is up to date and tracked commit should be set equal to base commit
     Ok(())
 }
 
@@ -119,8 +130,8 @@ fn main() {
                 let pool = SqlitePool::connect_with(options).await;
                 match pool {
                     Ok(db) => {
-                        let owo = app.path().resolve("migrations", BaseDirectory::Resource).unwrap();
-                        let m = Migrator::new(owo).await.unwrap();
+                        let migrations = app.path().resolve("migrations", BaseDirectory::Resource).unwrap();
+                        let m = Migrator::new(migrations).await.unwrap();
                         let res = m.run(&db).await;
                         match res {
                             Ok(()) => {},
