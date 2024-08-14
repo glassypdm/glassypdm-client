5a0565b35a68d02f679a3f06bed33d63100cf7e7
Joshua Tenorio <joshliam09@gmail.com>
2024-08-14 07:21:14 -0700

    project history page is hooked up sans pagination

--- src/routes/_app/_workbench/projects.$pid.history.tsx
+++ src/routes/_app/_workbench/projects.$pid.history.tsx
@@ -2,11 +2,26 @@ import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
 import { Pagination, PaginationContent, PaginationItem, PaginationLink, PaginationNext, PaginationPrevious } from '@/components/ui/pagination'
 import { ScrollArea } from '@/components/ui/scroll-area'
+import { useAuth } from '@clerk/clerk-react'
+import { useQuery } from '@tanstack/react-query'
 import { createFileRoute } from '@tanstack/react-router'
+import { invoke } from '@tauri-apps/api/core'
 
 export const Route = createFileRoute('/_app/_workbench/projects/$pid/history')({
-  // TODO add a route query param for offset
-  component: () => <History />
+  validateSearch: (search: Record<string, unknown>): any => {
+    // validate and parse the search params into a typed state
+    return {
+      offset: search.offset
+    }
+  },
+  component: () => <History />,
+  loader: async ({params }) => {
+    const url = await invoke("get_server_url")
+    return {
+      url: url,
+      pid: params.pid
+    }
+  }
 })
 
 /*
@@ -26,61 +41,19 @@ type CommitList struct {
 */
 
 interface CommitDescription {
-  commitId: number
-  commitNumber: number
-  numFiles: number
+  commit_id: number
+  commit_number: number
+  num_files: number
   author: string
   comment: string
   timestamp: number
 }
 
 interface Data {
-  numCommit: number
+  num_commits: number
   commits: CommitDescription[]
 }
-const data: Data = {
-  numCommit: 43,
-  commits: [
-    {
-      commitId: 43,
-      commitNumber: 43,
-      numFiles: 414,
-      author: "Josh Tenorio",
-      comment: "yap yap yap yap yap yap yap yap yap yap yap yap yap",
-      timestamp: 0
-    },
-    {
-      commitId: 42,
-      commitNumber: 42,
-      numFiles: 14,
-      author: "Josh Tenorio",
-      comment: "asdf sf",
-      timestamp: 0
-    },
-    {
-      commitId: 41,
-      commitNumber: 41,
-      numFiles: 111,
-      author: "Josh Tenorio",
-      comment: "jyjyk",
-      timestamp: 0
-    },    {
-      commitId: 40,
-      commitNumber: 40,
-      numFiles: 234,
-      author: "Josh Tenorio",
-      comment: "erherh",
-      timestamp: 0
-    },    {
-      commitId: 39,
-      commitNumber: 39,
-      numFiles: 1152,
-      author: "Josh Tenorio",
-      comment: "asdfasdf",
-      timestamp: 0
-    }
-  ]
-}
+
 
 interface DescriptionCardProps {
   commitDesc: CommitDescription
@@ -94,8 +67,8 @@ function DescriptionCard(props: DescriptionCardProps) {
     <Card className=''>
       <div className='flex flex-row items-center'>
         <CardHeader className='p-4 grow'>
-          <CardTitle className='text-lg'>{commitdesc.author} made {commitdesc.numFiles} changes</CardTitle>
-          <CardDescription className='w-[400px]'>Project Update {commitdesc.commitNumber} - {commitdesc.comment}</CardDescription>
+          <CardTitle className='text-lg'>{commitdesc.author} made {commitdesc.num_files} changes</CardTitle>
+          <CardDescription className='w-[400px]'>Project Update {commitdesc.commit_number} - {commitdesc.comment}</CardDescription>
         </CardHeader>
         <CardContent className='p-4 justify-self-end'>
           <Button>View</Button>
@@ -109,22 +82,55 @@ function DescriptionCard(props: DescriptionCardProps) {
 }
 
 function History() {
+  const { getToken } = useAuth();
+  const { url, pid } = Route.useLoaderData();
+  const { offset } = Route.useSearch();
+  const { isPending, isError, data, error } = useQuery({
+    queryKey: ["history", pid],
+    queryFn: async () => {
+      const endpoint = (url as string) + "/commit/select/by-project/" + pid + "?offset=" + offset;
+      const response = await fetch(endpoint, {
+        headers: { Authorization: `Bearer ${await getToken()}`},
+        method: "GET",
+        mode: "cors"
+      })
+
+      return response.json();
+    },
+  })
+
+  if(isPending) {
+    return (
+      <div>Loading</div>
+    )
+  }
+  console.log(data)
+  if(data.response != "success") {
+    return (
+      <div>An error occured</div>
+    )
+  }
+
+  // TODO handle pagination
+  
   return (
     <div className='flex flex-col items-center'>
       <ScrollArea className='h-96 space-y-2'>
         <div className='space-y-2 p-4'>
-        <DescriptionCard commitDesc={data.commits[0]} />
-        <DescriptionCard commitDesc={data.commits[1]} />
-        <DescriptionCard commitDesc={data.commits[2]} />
-        <DescriptionCard commitDesc={data.commits[3]} />
-        <DescriptionCard commitDesc={data.commits[4]} />
+          {
+            data.body.commits.map((commit: CommitDescription) => {
+              return (
+              <DescriptionCard commitDesc={commit} key={commit.commit_id} />
+              )
+            })
+          }
         </div>
       </ScrollArea>
-
+      {/*
       <Pagination>
         <PaginationContent>
           <PaginationItem>
-            <PaginationPrevious /> {/* className TODO set disabled if in-applicable*/}
+            <PaginationPrevious /> {/* className TODO set disabled if in-applicable*//*}
           </PaginationItem>
           <PaginationItem>
             <PaginationLink href="#">1</PaginationLink>
@@ -137,6 +143,7 @@ function History() {
           </PaginationItem>
         </PaginationContent>
       </Pagination>
+      */}
     </div>
   )
 }
