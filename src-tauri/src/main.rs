ffa2841cc46a36c888ebdd369a09f8c8cf9715bb
Joshua Tenorio <joshliam09@gmail.com>
2024-10-10 22:06:15 -0700

    update dependencies

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -67,7 +67,6 @@ fn main() {
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_shell::init())
-        .plugin(tauri_plugin_notification::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
         .setup(|app| {
             //let handle = app.handle().clone();
